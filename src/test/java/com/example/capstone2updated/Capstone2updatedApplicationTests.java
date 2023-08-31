31990830b76d077bc313c7f2529270b83f771ccd
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-31 06:35:11 +0300

    add project

---
+++ src/test/java/com/example/capstone2updated/Capstone2updatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone2updated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone2updatedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
